e0374b9abb3b0c0d3351b5a88e4190be5b5ac84d
Julio Pérez <jperez@tegik.com>
2015-04-13 18:22:07 -0500

    Cobranza Correcciones

--- app/controllers/api/v1/documentoscobranzas_controller.rb
+++ app/controllers/api/v1/documentoscobranzas_controller.rb
@@ -7,7 +7,8 @@ module Api
       def index
         @user = current_usuario
         #order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "POB_VistaFacturasPortal?_where=taxid=%27" + @user.rfc + "%27"
-        order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "/openbravo/org.openbravo.service.json.jsonrest/pob_documentocobranza?_where=taxid=%27" + @user.rfc + "%27"
+        order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "/openbravo/org.openbravo.service.json.jsonrest/pob_documentocobranza?_where=taxid=%27" + @user.rfc + "%27&orderBy=dateinvoiced"
+        puts order_link
     	  response = RestClient.get order_link 
       	respond_to do |format|
       		format.json {render json: response}
