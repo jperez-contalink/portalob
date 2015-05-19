09c3f918426586cbbd89276f3425b41b4042281f
Julio Pérez <jperez@tegik.com>
2015-05-19 09:58:53 -0500

    Cobranza Revisión #1

--- app/controllers/api/v1/reporte_estado_cuenta_controller.rb
+++ app/controllers/api/v1/reporte_estado_cuenta_controller.rb
@@ -7,7 +7,7 @@ module Api
       def index
         @user = current_usuario
         #order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "POB_VistaEstadoCuenta?_where=taxid=%27" + @user.rfc + "%27&_sortBy=fecha"
-        order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "/openbravo/org.openbravo.service.json.jsonrest/POB_VistaEstadoCuenta?_where=taxid=%27" + @user.rfc + "%27&_sortBy=fecha"
+        #order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "/openbravo/org.openbravo.service.json.jsonrest/POB_VistaEstadoCuenta?_where=taxid=%27" + @user.rfc + "%27&_sortBy=fecha"
         order_link = "http://" + @user.empresa.usuariosob + ":" + @user.empresa.passob + "@" + @user.empresa.urlwebservice + "/openbravo/org.openbravo.service.json.jsonrest/POB_VistaEstadoCuenta?_where=bpartner=%27" + @user.partner_id + "%27&_sortBy=fecha"
         puts order_link
         response = RestClient.get order_link 
