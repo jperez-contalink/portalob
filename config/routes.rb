8adf8b2e62b4b612a2a1c05012a5d2fd9b52c932
Julio Pérez <jperez@tegik.com>
2015-05-20 10:27:14 -0500

    PASSENGER CORRECCION

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Rails.application.routes.draw do
   get "cobranza" => "pages#cobranza"
   get "unauthorized" => "pages#unauthorized"
   get "imagenes" => "pages#importar_imagenes"
-
+  get "clientes" => "pages#clientes"
  
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
@@ -80,6 +80,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :metodopagos
+    end  
+  end
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      resources :customers
     end  
   end
 
