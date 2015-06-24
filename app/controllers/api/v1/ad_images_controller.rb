3212278444205ab25324a9e5660e809b5b8b538e
Jesus Lerma <shuyojl@gmail.com>
2015-06-24 08:59:20 -0500

    Display product images in product grid

--- app/controllers/api/v1/ad_images_controller.rb
+++ app/controllers/api/v1/ad_images_controller.rb
@@ -1,7 +1,7 @@
 	
 class Api::V1::AdImagesController < ApplicationController
 	def show
-		order_link = "http://csalinas-tegik:KopoTegik@ec2-54-158-147-120.compute-1.amazonaws.com:8080/openbravo/org.openbravo.service.json.jsonrest/ADImage/F43F1BB08A504B859FC6A4ACFC9431FB"
+		order_link = "http://csalinas-tegik:KopoTegik@ec2-54-158-147-120.compute-1.amazonaws.com:8080/openbravo/org.openbravo.service.json.jsonrest/ADImage/"+params[:id]
     response = RestClient.get order_link 
     json_response = JSON.parse response, symbolize_names: true
 
--- vendor/assets/javascripts/jquery.catalogo.js
+++ vendor/assets/javascripts/jquery.catalogo.js
@@ -39,6 +39,7 @@
             var search = '<div class="input-group"><span class="input-group-addon glyphicon glyphicon-search"></span><input type="text" class="form-control btn_search" aria-describedby="basic-addon1" id="txt_search" autofocus></div>';
             var tabla = search + '<br><div width="100%">';
             var style="";
+            var product_ids = [];
             for (var key in jsonRows) {
                 var obj = jsonRows[key]; 
                 var inventario = parseFloat((obj.Inventario).replace(" Piezas", ""));
@@ -58,12 +59,24 @@
                         inventarioShow = obj.Inventario;
                     }
 
-                    tabla += '<a href="javascript:agregar();" class="addProduct" id="' + obj.Producto_ID + '"><div class="itemA" align="left"><div align="center" class="bgImage" style="background-image:url('+obj.Image+');"><br><br><br><br><br><br></div><br><b>'+obj.Nombre +'</b><br>$'+(parseFloat(obj.Precioshow)).toFixed(2)+' ' + pextra + '<br>'+inventarioShow+'</div></a>';
+                    tabla += '<a href="javascript:agregar();" class="addProduct"'+
+                    ' id="' + obj.Producto_ID + '">'+
+                    '<div class="itemA" align="left">'+
+                    '<div align="center" class="bgImage"'+
+                     'data-product-id='+obj.Producto_ID+
+                     ' data-image-id='+obj.imageId+'>'+
+                    '<br><br><br><br><br><br></div><br><b>'+
+                    obj.Nombre +'</b><br>$'+
+                    (parseFloat(obj.Precioshow)).toFixed(2)+' ' + pextra + '<br>'
+                    +inventarioShow+'</div></a>';
                     style="";
+                    product_ids.push(obj.Producto_ID);
                 }
             }
             tabla += "</div>";
             document.getElementById("cat_pro").innerHTML = tabla;
+            AdImage.init();
+            AdImage.getAdImage(".bgImage");
             // PINTAR PAGINACIÓN
             priv.drawPagination(pData);
             
